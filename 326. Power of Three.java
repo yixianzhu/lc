be4a3e14028c90da6e11cfc654c3d622f026f5cd
yixianzhu <zhuyixianzyx@gmail.com>
2016-01-09 17:28:56 -0500

    Update 326. Power of Three.java

--- 326. Power of Three.java
+++ 326. Power of Three.java
@@ -2,3 +2,15 @@
 public boolean isPowerOfThree(int n) {
     return (Math.log10(n) / Math.log10(3)) % 1 == 0;
 }
+
+//recursion
+public boolean isPowerOfThree(int n) {
+    return n>0 && (n==1 || (n%3==0 && isPowerOfThree(n/3)));
+}
+
+//iterate
+public boolean isPowerOfThree(int n) {
+    if(n>1)
+        while(n%3==0) n /= 3;
+    return n==1;
+}
