6ad11543a207c2f3db56a39bc5163be1a731ce83
yixianzhu <zhuyixianzyx@gmail.com>
2016-01-09 21:31:15 -0500

    Create 125. Valid Palindrome.java

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,37 @@
+//correct solution
+/* I have the same thought with this method
+however I miss some details.
+*/
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {//
+            return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;//need to use char, because this is not an array!
+        while(head <= tail) {
+            cHead = s.charAt(head);//charAt method
+            cTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(cHead)) {//isLetterOrDigit method...
+                head++;
+            } else if(!Character.isLetterOrDigit(cTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {//toLowerCae method
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+
+        return true;
+    }
+}
+
+/*
+cannot use 'for(int head = 0, tail = s.length() - 1; head <= tail; head++, tail--){'
+it will be a mess.....
+because cHead = s.charAt(head); cannot be used anymore
+once used, the problem is that the head in the brackets won't change if head++ or tail--
+
