a96c3099c996f685e17b6fcc39adb55736802c09
yixianzhu <zhuyixianzyx@gmail.com>
2016-01-21 11:40:10 -0500

    Update 328. Odd Even Linked List.java

--- 328. Odd Even Linked List.java
+++ 328. Odd Even Linked List.java
@@ -6,16 +6,20 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+ //use two pointers to do
+ //not suitable for recurion, should use while loop here
 public class Solution {
     public ListNode oddEvenList(ListNode head) {
         if(head==null||head.next==null) return head;
         ListNode odd=head,ehead=head.next,even=ehead;
+        
         while(even!=null&&even.next!=null){
             odd.next=even.next;
             odd=odd.next;
             even.next=odd.next;
             even=even.next;
         }
+        
         odd.next=ehead;
         return head;
     }
