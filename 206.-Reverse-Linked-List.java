038b1ab6502cf8bc9e4455228ff6c51265361396
yixianzhu <zhuyixianzyx@gmail.com>
2016-01-20 23:40:44 -0500

    Create 206.-Reverse-Linked-List.java

---
+++ 206.-Reverse-Linked-List.java
@@ -0,0 +1,70 @@
+// iterative solution
+
+public ListNode reverseList(ListNode head) {
+    ListNode newHead = null;
+    while(head != null){
+        ListNode next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+}
+
+
+// iterative solution
+
+public ListNode reverseList(ListNode head) {
+    ListNode newHead = null;
+    while(head != null){
+        ListNode next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+}
+
+//iterate
+public ListNode reverseList(ListNode head) {
+    ListNode first = null;
+    ListNode tail = head;
+    while (tail != null) {
+        ListNode tmp = tail;
+        tail = tail.next;
+        tmp.next = first;
+        first = tmp;
+    }
+    return first;
+}
+
+// Recursion:
+
+public ListNode reverseList(ListNode head) {
+    return helper(null, head);
+}
+
+ListNode helper(ListNode reversed, ListNode remaining) {
+    if(remaining==null) return reversed;
+    ListNode tmp = remaining.next;
+    remaining.next = reversed;
+
+    return helper(remaining, tmp);
+}
+// Iteration:
+
+public ListNode reverseList(ListNode head) {
+    if(head==null) return head;
+
+    ListNode newhead = new ListNode(0);
+    newhead.next = head;
+
+    while(head.next!=null) {
+        ListNode tmp = head.next;
+        head.next = head.next.next;
+
+        tmp.next = newhead.next;
+        newhead.next = tmp;
+    }
+    return newhead.next;
+}
