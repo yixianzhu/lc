4a9dbdc777888225ae780f9440c51de4f8ed6d3c
yixianzhu <zhuyixianzyx@gmail.com>
2016-01-22 15:04:59 -0500

    Update 00001.java

--- 00001.java
+++ 00001.java
@@ -11,3 +11,20 @@ String.valueOf(char) // which also works.
 //As others have noted, string concatenation works as a shortcut as well:
 String s = "" + 's';
 
+///////comparison///////////
+// These two have the same value
+new String("test").equals("test") // --> true 
+
+// ... but they are not the same object
+new String("test") == "test" // --> false 
+
+// ... neither are these
+new String("test") == new String("test") // --> false 
+
+// ... but these are because literals are interned by 
+// the compiler and thus refer to the same object
+"test" == "test" // --> true 
+
+// checks for nulls and calls .equals()
+Objects.equals("test", new String("test")) // --> true
+Objects.equals(null, "test") // --> false
