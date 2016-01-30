0a7a5bca869ea706d3fc3c4b40faf0f3ea811603
yixianzhu <zhuyixianzyx@gmail.com>
2016-01-30 14:53:10 -0500

    Update 00001.java

--- 00001.java
+++ 00001.java
@@ -7,6 +7,8 @@ System.out.println("x=" + x);
 //++from char to string++
 //You can use 
 Character.toString(char)//Note that this method simply returns a call to 
+//还有
+Interger.toString(int)
 String.valueOf(char) // which also works.
 //As others have noted, string concatenation works as a shortcut as well:
 String s = "" + 's';
