7050ce3b137f8b836027bffb6a66c84c73da1525
yixianzhu <zhuyixianzyx@gmail.com>
2016-01-30 21:05:23 -0500

    Update 324. Wiggle Sort II.java

--- 324. Wiggle Sort II.java
+++ 324. Wiggle Sort II.java
@@ -16,8 +16,6 @@ public void wiggleSort(int[] nums) {
 
 
 
-Java
-
 public class Solution {
     public void wiggleSort(int[] nums) {
         Arrays.sort(nums);
@@ -33,16 +31,3 @@ public class Solution {
 }
 
 
-public class Solution {
-    public void wiggleSort(int[] nums) {
-        Arrays.sort(nums);
-        int[] temp = new int[nums.length];
-        int s = (nums.length + 1) >> 1, t = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            temp[i] = (i & 1) == 0 ?  nums[--s] : nums[--t] ;
-        }
- 
-        for (int i = 0; i < nums.length; i++)
-            nums[i] = temp[i];
-    }
-}
