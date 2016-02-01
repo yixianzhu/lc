7905cbf41cc33e1d0a3e043a516c21692bcfe8de
yixianzhu <zhuyixianzyx@gmail.com>
2016-02-01 17:51:24 -0500

    Update 118. Pascal's Triangle.java

--- 118. Pascal's Triangle.java
+++ 118. Pascal's Triangle.java
@@ -12,3 +12,28 @@ public class Solution {
         return allrows;
     }
 }
+
+//更好理解更正常的做法
+/*
+ * Pascal’s Triangle I,空间复杂度O(n²),时间复杂度O(n²)
+ */
+public static List<List<Integer>> generate1(int numRows) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    // 不合法的输入
+    if (numRows <= 0)
+        return result;
+    List<Integer> pre = new ArrayList<Integer>();
+    pre.add(1);
+    result.add(pre);
+    for (int i = 2; i <= numRows; i++) {
+        List<Integer> cur = new ArrayList<Integer>();
+        cur.add(1); // 开头元素
+        for (int j = 0; j < pre.size() - 1; j++) {
+            cur.add(pre.get(j) + pre.get(j + 1)); // 中间位置
+        }
+        cur.add(1);// 末尾元素
+        result.add(cur);
+        pre = cur;//记得这步
+    }
+    return result;
+}
