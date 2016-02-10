8af492047c2702bc50693b041aef1eba315f0468
yixianzhu <zhuyixianzyx@gmail.com>
2016-02-10 09:25:03 -0500

    Update 125. Valid Palindrome.java

--- 125. Valid Palindrome.java
+++ 125. Valid Palindrome.java
@@ -34,4 +34,13 @@ cannot use 'for(int head = 0, tail = s.length() - 1; head <= tail; head++, tail-
 it will be a mess.....
 because cHead = s.charAt(head); cannot be used anymore
 once used, the problem is that the head in the brackets won't change if head++ or tail--
+*/
 
+//another method use Regex, which is slower because it uses a new String
+public class Solution {
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
+    }
+}
