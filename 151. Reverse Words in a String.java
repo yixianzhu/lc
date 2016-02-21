67f571a797804d7b55b19c5c807732b214f57870
yixianzhu <zhuyixianzyx@gmail.com>
2016-02-21 16:15:55 -0500

    Update 151. Reverse Words in a String.java

--- 151. Reverse Words in a String.java
+++ 151. Reverse Words in a String.java
@@ -3,7 +3,7 @@ public class Solution {
         String[] split = s.trim().split("\\s+");//trim is important!
         String result = "";
         if(split.length>0){//should not forget this line
-            for(int i=split.length-1; i>0; i--){
+            for(int i=split.length-1; i>0; i--){//这个for循环到i=1停止，因为最后一个单词之后不用加空格
                 result += split[i] + " ";
             }
             result += split[0];          
