bb984c9e54fe13055121eb506c0f25e372ce56e6
yixianzhu <zhuyixianzyx@gmail.com>
2016-02-23 12:38:29 -0500

    Update 111. Minimum Depth of Binary Tree.java

--- 111. Minimum Depth of Binary Tree.java
+++ 111. Minimum Depth of Binary Tree.java
@@ -10,6 +10,7 @@
  //Logic here is that if you find a node with either of right or left is absent
  //return the maximum of left/right because one of them is 0 anyway.
  //correct solution
+//DFS
 public class Solution {
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
@@ -17,4 +18,36 @@ public class Solution {
             return 1 + Math.max(minDepth(root.left), minDepth(root.right));
         else return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
+}
+
+//BDS
+//level-order traversal and record current level depth, 
+//when meet a node which both child is null then return, no need to go farther
+public class Solution {
+public int minDepth(TreeNode root) {
+    if (root == null)
+        return 0;
+    int depth = 1;
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    TreeNode temp,magic = new TreeNode(0);
+    queue.add(root);
+    queue.add(magic);
+    while(!queue.isEmpty()){
+        temp = queue.poll();
+        if(temp.equals(magic)){
+            if(!queue.isEmpty()){
+                depth++;
+                queue.add(magic);
+            }
+            continue;
+        }
+        if(temp.left == null && temp.right == null)
+            return depth;
+        if(temp.left != null)
+            queue.add(temp.left);
+        if(temp.right != null)
+            queue.add(temp.right);
+    }
+    return depth;
+}
 }
