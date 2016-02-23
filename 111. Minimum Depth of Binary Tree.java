53092deaf9c2232c928eff26ecf220c59c2aec23
yixianzhu <zhuyixianzyx@gmail.com>
2016-02-23 12:43:08 -0500

    Update 111. Minimum Depth of Binary Tree.java

--- 111. Minimum Depth of Binary Tree.java
+++ 111. Minimum Depth of Binary Tree.java
@@ -23,6 +23,29 @@ public class Solution {
 //BDS
 //level-order traversal and record current level depth, 
 //when meet a node which both child is null then return, no need to go farther
+ public int minDepth(TreeNode root) {
+    if(root==null) return 0;
+
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.add(root);
+    TreeNode endOfLevel = root;
+    int depth = 1;
+
+    while( !queue.isEmpty() ) {
+        TreeNode node = queue.remove();
+        if(node.left==null && node.right==null) return depth;
+        if(node.left!=null) queue.add(node.left);
+        if(node.right!=null)  queue.add(node.right);
+        if(node == endOfLevel) {
+            endOfLevel = node.right==null ? node.left : node.right;
+            depth++;
+        }
+    }
+
+    return depth;
+}
+
+//another
 public class Solution {
 public int minDepth(TreeNode root) {
     if (root == null)
