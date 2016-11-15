81c16b04100d9ce3dbeac3d38917c6c74bdf6077
yixianzhu <zhuyixianzyx@gmail.com>
2016-11-14 21:16:25 -0500

    Update 21. Merge Two Sorted Lists.java

--- 21. Merge Two Sorted Lists.java
+++ 21. Merge Two Sorted Lists.java
@@ -24,3 +24,30 @@ public class Solution {
         return newhead;//这里return的是recursion 最外层的newhead，也就是第一个head
     }
 }
+
+// iteration
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    ListNode head = new ListNode(0);
+    ListNode p = head;
+ 
+    while(l1!=null||l2!=null){
+        if(l1!=null&&l2!=null){
+            if(l1.val < l2.val){
+                p.next = l1;
+                l1=l1.next;
+            }else{
+                p.next=l2;
+                l2=l2.next;
+            }
+            p = p.next;
+        }else if(l1==null){
+            p.next = l2;
+            break;
+        }else if(l2==null){
+            p.next = l1;
+            break;
+        }
+    }
+ 
+    return head.next;
+}
